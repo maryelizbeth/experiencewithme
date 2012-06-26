29659879f427fc2698fd372c7a6f8554cb1a5c6f
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-26 18:16:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Experiencewithme::Application.config.secret_token = '4cc13e9976e5a25700c288282c2988469461f04200fee0783fc4a5dc5ecb3607afe2c211dd3d23aaa40d1d21c7fe6bbc92ce559828a6fe13ad0f9069a5faf63d'

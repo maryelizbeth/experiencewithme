be53510a3f1681b95f748352c7f3ca178aab2d3e
Mary Cutrali <mary.cutrali@gmail.com>
2012-07-02 17:44:09 -0400

    user can not edit calendar events

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -3,7 +3,7 @@ class InvitationsMailer < ActionMailer::Base
   default from: "experienceinvitations@experiencewit.me"
 
   def invitation_email(current_user, friend, adventure)
-    mail(:to => "#{current_user.invitations.friend_email}", 
-          :subject => "You're Invited to Join #{user.name} on a #{current_user.user_adventures.adventure.name} Adventure.")
+    mail(:to => "#{current_user.invitations.last.friend_email}", 
+          :subject => "You're Invited to Join #{current_user.name} on a #{current_user.user_adventures.adventure.name} Adventure.")
   end 
 end

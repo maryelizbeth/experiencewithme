78feb5e3f3bd02bfdc56e8a62ae09abc7768c8ff
Mary Cutrali <mary.cutrali@gmail.com>
2012-07-02 13:49:03 -0400

    invitations mailer and friends syncing

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,9 +7,9 @@ class InvitationsController < ApplicationController
 
   def create 
     @invitation = Invitation.new(params[:invitation])
-    @invitation.sender = current_user
+    @invitation.user = current_user
     if @invitation.save 
-      if logged_in?
+      if current_user
         InvitationsMailer.invitation_email(@user, @friend, @adventure).deliver
         flash[:notice] = "Your friends have been invited to join you on this adventure."
         redirect_to root_url

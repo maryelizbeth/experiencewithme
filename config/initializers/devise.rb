2914c6c475d976fdb13d9dc0d3ccae671c0e99f4
Mary Cutrali <mary.cutrali@gmail.com>
2012-07-02 09:28:16 -0400

    setting up directory for deployment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,4 @@
-require_relative 'oauth_keys'
+require 'config/initializers/oauth_keys'
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
---
+++ config/initializers/oauth_keys.rb
@@ -0,0 +1,5 @@
+unless defined?(GOOGLE_KEY)
+  GOOGLE_KEY = '1010028708675.apps.googleusercontent.com'
+  GOOGLE_SECRET = 'iyESKWws8SvPP4EEzAzFqURd'
+end
+

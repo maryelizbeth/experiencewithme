52bd9619b3ede3dcd2a0e010707769fb2a6495ed
Mary Cutrali <mary.cutrali@gmail.com>
2012-07-02 11:42:34 -0400

    fix syntax

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -3,6 +3,7 @@ class InvitationsMailer < ActionMailer::Base
   default from: "experienceinvitations@experiencewit.me"
 
   def invitation_email(user, friend, adventure)
-    mail (:to => "#{friend.name} <#{friend.email}>", :subject => "You're Invited to Join #{user.name} on a #{adventure.title} Adventure.")
+    mail(:to => "#{friend.name} <#{friend.email}>", 
+          :subject => "You're Invited to Join #{user.name} on a #{adventure.title} Adventure.")
   end 
 end
